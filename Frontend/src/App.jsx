a9fbf7bece580b0edd380a59217babda1ce69d8e
Saurav Kumar <saurav.sde.cs@gmail.com>
2025-06-03 02:27:44 +0530

    created HomePage and Discuss admin Panel

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,36 @@
-import {Routes, Route} from 'react-router';
+import {Routes, Route, Navigate} from 'react-router';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Homepage from './pages/Homepage';
+import { checkAuth } from './authSlice';
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from 'react';
 
 function App() {
-  return (
+
+  // code to check user is authenticated or not
+  const {isAuthenticated, loading} = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    dispatch(checkAuth());
+  },[dispatch]); // as we want to run it only one time, we can keep it empty also, but we have written here a constant, which is better
+
+  if(loading)
+    return (
+      <div className='min-h-screen flex items-center justify-center'>
+        <span className='loading loading-spinner loading-lg'></span>
+      </div>
+  )
+
+
+  return ( 
     <>
       <Routes>
-        <Route path='/' element={<Homepage></Homepage>}></Route>
-        <Route path='/login' element={<Login></Login>}></Route>
-        <Route path='/signup' element={<Signup></Signup>}></Route>
+        <Route path='/' element={isAuthenticated?<Homepage></Homepage>:<Navigate to="/signup"/>}></Route>
+        <Route path='/login' element={isAuthenticated?<Navigate to="/"/>:<Login></Login>}></Route>
+        <Route path='/signup' element={isAuthenticated?<Navigate to="/"/>:<Signup></Signup>}></Route>
+        <Route path='/admin' element={<AdminPanel/>}></Route>
       </Routes>
     </>
   )
