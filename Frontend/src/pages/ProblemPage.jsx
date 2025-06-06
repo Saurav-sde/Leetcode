ecd7fb9654274c4fff131f8482180a934aad8b64
Saurav Kumar <saurav.sde.cs@gmail.com>
2025-06-07 00:06:13 +0530

    integrated AI

--- Frontend/src/pages/ProblemPage.jsx
+++ Frontend/src/pages/ProblemPage.jsx
@@ -4,6 +4,7 @@ import Editor from '@monaco-editor/react';
 import { useParams } from 'react-router';
 import axiosClient from "../utils/axiosClient";
 import SubmissionHistory from '../components/SubmissionHistory';
+import ChatAi from '../components/ChatAi';
 
 
 const ProblemPage = () => {
@@ -326,6 +327,12 @@ const ProblemPage = () => {
           >
             Submissions
           </button>
+          <button 
+            className={`tab ${activeLeftTab === 'chatAI' ? 'tab-active' : ''}`}
+            onClick={() => setActiveLeftTab('chatAI')}
+          >
+            ChatAI
+          </button>
         </div>
 
         {/* Left Content */}
@@ -400,6 +407,15 @@ const ProblemPage = () => {
                   <h2 className="text-xl font-bold mb-4">My Submissions</h2>
                   <div className="text-gray-500">
                     <SubmissionHistory problemId={problemId}></SubmissionHistory>
+                  </div>
+                </div>
+              )}
+
+              {activeLeftTab === 'chatAI' && (
+                <div className="prose max-w-none">
+                  <h2 className="text-xl font-bold mb-4">CHAT with AI</h2>
+                  <div className="whitespace-pre-wrap text-sm leading-relaxed">
+                    <ChatAi problem={problem}></ChatAi>
                   </div>
                 </div>
               )}
